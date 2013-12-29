ee957623fec21b829e2c25cf1b343698449d309a
splitice <mat999@gmail.com>
2013-12-29 17:42:00 +1100

    tested working

--- src/Radical/Web/Sass.php
+++ src/Radical/Web/Sass.php
@@ -2,6 +2,27 @@
 namespace Radical\Web;
 
 class Sass {
+	private static function getFunctions($extensions) {
+		$output = array ();
+		if (! empty ( $extensions )) {
+			foreach ( $extensions as $extension ) {
+				$name = explode ( '/', $extension, 2 );
+				$namespace = ucwords ( preg_replace ( '/[^0-9a-z]+/', '_', strtolower ( array_shift ( $name ) ) ) );
+				global $BASEPATH;
+				$sass_path = $BASEPATH.'/vendor/richthegeek/phpsass/tree/Extensions/';
+				$extensionPath = $sass_path.'/' . $namespace . '/' . $namespace . '.php';
+				if (file_exists ( $extensionPath )) {
+					require_once ($extensionPath);
+					$namespace = $namespace . '::';
+					$function = 'getFunctions';
+					$output = array_merge ( $output, call_user_func ( $namespace . $function, $namespace ) );
+				}
+			}
+		}
+	
+		return $output;
+	}
+	
 	static function render($file){
 		$ext = pathinfo($file, PATHINFO_EXTENSION);
 		$bn = basename ( $file );
